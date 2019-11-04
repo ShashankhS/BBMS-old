11ecf1aecbab181ebd1e4c0bb32f985b70e0ea0b
ShashankhS <shashankh.s22@gmail.com>
2019-11-04 17:39:30 +0530

    Added connector for return cx_Oracle object

---
+++ controller/connector.py
@@ -0,0 +1,11 @@
+import cx_Oracle
+
+def connector():
+	ip = '192.168.1.42'
+	port = 1521
+	SID = 'system'
+	dsn_tns = cx_Oracle.makedsn(ip, port, SID)
+	user_name = 'system'
+	password = 'system'
+	con = cx_Oracle.connect(user_name, password, dsn_tns)
+	return con
