9099b0c6e2bd46ce4f8dd0cdbbe2d84c2e5db60d
ShashankhS <shashankh.s22@gmail.com>
2019-12-01 14:43:35 +0530

    Preparation for next module

---
+++ controller/checkList.py
@@ -0,0 +1,20 @@
+import os
+
+import sys
+import hashlib
+sys.path.append('C:\\page\\bbms\\controller')
+import login_Check
+
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+
+try:
+    import ttk
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+    py3 = True
+
+import login_support
